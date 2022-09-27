cf293d58f55dcc5c18af8501a81cd8ecda20ee6f
md_mumtaj_alam <mmumtajalam987@gmail.com>
2022-09-27 22:55:16 +0530

    update

--- src/components/Feeds.js
+++ src/components/Feeds.js
@@ -3,12 +3,15 @@ import React, { useEffect, useRef, useState } from "react";
 import Comments from "./Comments";
 import Right from "./comman/Right";
 import Left from "./comman/Left";
+
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const Feeds = () => {
   const [postItem, setPostItem] = useState([]);
   const [successPost, setSuccessPost] = useState(false);
+  const [toggleEdit, setToggleEdit] = useState(true);
+  const [editPost, setEditPost] = useState({});
   const navigate = useNavigate();
   const loginData = useSelector((state) => state.login.loginDataRedux);
   console.log("loginData.....", loginData);
@@ -26,7 +29,7 @@ const Feeds = () => {
       if (response.status === 201) {
         setSuccessPost(true);
       }
-      console.log(response);
+      //console.log(response);
       text.current.value = "";
       setTimeout(() => {
         setSuccessPost(false);
@@ -34,11 +37,37 @@ const Feeds = () => {
     }
   };
 
+  const editFn = (feedid) => {
+    //console.log("getting feed id....", feedid);
+    let newEditText = postItem.find((item) => {
+      return item.feedid === feedid;
+    });
+    console.log(newEditText);
+    setToggleEdit(false);
+    setEditPost(newEditText);
+    text.current.value = newEditText.itemText;
+  };
+
+  const savePost = async () => {
+    const newUrl = `https://calm-meadow-38443.herokuapp.com/feeditems/edititem/${editPost.feedid}`;
+    console.log(newUrl);
+    const tempObj = {};
+    tempObj.feedid = editPost.feedid;
+    tempObj.userid = editPost.userid;
+    tempObj.name = editPost.name;
+    tempObj.itemText = text.current.value;
+    const response = await axios.put(newUrl, tempObj);
+    if (response.status === 201) {
+      console.log(editPost);
+      setToggleEdit(true);
+      text.current.value = "";
+    }
+  };
   const callItemApi = async () => {
     const url = "https://calm-meadow-38443.herokuapp.com/feeditems/allitems";
     try {
       const response = await axios.get(url);
-      console.log(response);
+      //console.log(response);
       setPostItem(response.data.reverse());
     } catch (err) {
       console.log(err);
@@ -53,21 +82,35 @@ const Feeds = () => {
 
   useEffect(() => {
     callItemApi();
-  }, [successPost]);
+  }, [successPost, toggleEdit]);
   return (
     <div className="d-flex">
       <Left />
       <section className="col-8 m-2" style={{ backgroundColor: "gray" }}>
+        <div className="d-flex">
+          <img
+            src="https://i.pinimg.com/736x/25/78/61/25786134576ce0344893b33a051160b1.jpg"
+            className="profile_pic"
+            alt="profile"
+          />
+          <p className="profile_name">{loginData && loginData.name}</p>
+        </div>
         <div className="d-flex flex-column rounded text-center">
           <textarea
-            className="m-2 border rounded"
+            className="m-2 border rounded p-2"
             placeholder="what's on your mind..."
             ref={text}
           ></textarea>
           <span className="align-self-end">
-            <button className="btn btn-sm btn-info mx-2" onClick={submitPost}>
-              Post
-            </button>
+            {toggleEdit ? (
+              <button className="btn btn-sm btn-info mx-2" onClick={submitPost}>
+                Post
+              </button>
+            ) : (
+              <button className="btn btn-sm btn-info mx-2" onClick={savePost}>
+                save
+              </button>
+            )}
           </span>
         </div>
         {successPost && (
@@ -76,7 +119,10 @@ const Feeds = () => {
           </p>
         )}
 
-        <div className="d-flex flex-column m-2 border border-dark rounded">
+        <div
+          className="d-flex flex-column m-2 border border-dark rounded"
+          style={{ backgroundColor: "white" }}
+        >
           {postItem &&
             postItem.map((item) => (
               <div
@@ -84,19 +130,34 @@ const Feeds = () => {
                 className="m-1 p-1 border border-dark rounded"
                 style={{ backgroundColor: "white" }}
               >
-                <div>
-                  <div className="d-flex">
-                    <img
-                      src="https://cdn-icons-png.flaticon.com/512/21/21294.png"
-                      alt="profile"
-                      style={{ height: "20px", width: "20px" }}
-                    />
-                    <p style={{ fontWeight: "bold" }}>{item.name}</p>
+                <div className="m-1">
+                  <div className="d-flex justify-content-between">
+                    <span className="d-flex">
+                      <img
+                        src="https://cdn-icons-png.flaticon.com/512/21/21294.png"
+                        alt="profile"
+                        style={{ height: "20px", width: "20px" }}
+                      />
+                      <p style={{ fontWeight: "bold" }}>{item.name}</p>
+                    </span>
+                    {loginData.userid === item.userid && (
+                      <div>
+                        <i
+                          className="fa-solid fa-pen-to-square option_icon"
+                          onClick={() => {
+                            editFn(item.feedid);
+                          }}
+                        ></i>
+                        <i className="fa-solid fa-trash option_icon"></i>
+                      </div>
+                    )}
                   </div>
-                  {item.itemText && <p>{item.itemText}</p>}
+                  {item.itemText && (
+                    <p className="ps-3 post-text">{item.itemText}</p>
+                  )}
                   {item.itemImage && <img src={item.itemImage} alt="post" />}
                 </div>
-                <Comments id={item.feedid} />
+                <Comments id={item.id} />
               </div>
             ))}
         </div>
